0b05a18cde5c27c31124c38a905d9d190ec07b4b
SvetlanaMedvedeva73 <sweta.medvedev16@yandex.ru>
2022-12-16 15:22:00 +0300

    dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов
+ в каждом столбце.
+ */
+
+ int[,] array = new int[4, 4];
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+          array[i, j] = new Random().Next(20);
+          Console.Write($"{array[i, j]} ");
+     }
+     Console.WriteLine();
+ }
+
+ for (int j = 0; j < array.GetLength(1); j++)
+ {
+     int sum = 0;
+     for (int i = 0; i < array.GetLength(0); i++)
+         sum += array[i, j];
+     Console.Write($"{Math.Round((double)sum / array.GetLength(0), 1)} ");
+}
+
+ 
+
